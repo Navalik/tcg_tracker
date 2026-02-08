cce19a6dbe3a6a1151414ed46dcc3435ec0691af
Navalik <damore.mirko@gmail.com>
2026-02-08 20:01:52 +0100

    fix1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.tcg_tracker"
+    namespace = "com.navalik.bindervault"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -15,13 +15,15 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.tcg_tracker"
+        applicationId = "com.navalik.bindervault"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/navalik/bindervault/MainActivity.kt
+++ android/app/src/main/kotlin/com/navalik/bindervault/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.tcg_tracker
+package com.navalik.bindervault
 
 import io.flutter.embedding.android.FlutterActivity
 
